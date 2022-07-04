ba3311f306d2f1fede2d4193b63cdc8ffea5c19c
alcor0805 <106061646+alcor0805@users.noreply.github.com>
2022-07-04 19:12:20 +0800

    0704 update 人物事件按鈕

--- finalgame/finalgame/Assets/Script/person_walk.cs
+++ finalgame/finalgame/Assets/Script/person_walk.cs
@@ -6,15 +6,30 @@ namespace Alcor
         #region 資料
         private Rigidbody2D circle;
         private Vector3 move;
+<<<<<<< Updated upstream
         
         #endregion
         #region 功能
+=======
+
+        #endregion
+        #region 功能
+        private void OnDisable()
+        {
+            circle.MovePosition(circle.transform.position);
+            circle.velocity = Vector3.zero;
+        }
+>>>>>>> Stashed changes
         private void run() 
         {
             float h = Input.GetAxisRaw("Horizontal");
             float v = Input.GetAxisRaw("Vertical");
             move.Set(h,0f,v);
+<<<<<<< Updated upstream
             move = move.normalized * 50 * Time.deltaTime;
+=======
+            move = move.normalized * 10 * Time.deltaTime;
+>>>>>>> Stashed changes
             circle.MovePosition(circle.transform.position+move);
         }
         #endregion
--- finalgame/finalgame/Assets/Script/touch.cs
+++ finalgame/finalgame/Assets/Script/touch.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+<<<<<<< Updated upstream
 namespace Alcor 
+=======
+using TMPro;
+namespace Alcor
+>>>>>>> Stashed changes
 {
     public class touch : MonoBehaviour
     {
         #region 資料
+<<<<<<< Updated upstream
         private GameObject square;
         [SerializeField]
         private Text dialogtext;
@@ -20,6 +26,47 @@ namespace Alcor
             {
                 dialog.SetActive (true);
                 dialogtext.text="can you help me??";
+=======
+
+        private GameObject square;
+        [SerializeField]
+        private person_walk person_Walk;
+        [SerializeField]
+        private TextMeshProUGUI dialogtext;
+        [SerializeField]
+        private helpcanva helpcanva;
+        [SerializeField]
+        private CanvasGroup canvas;
+        #endregion
+        #region 功能
+        public void YesEvent()
+        {
+            person_Walk.enabled = true;
+            canvas.alpha = 0;
+            canvas.interactable = false;
+            canvas.blocksRaycasts = false;
+        }
+        public void NOEvent()
+        {
+            person_Walk.enabled = true;
+            canvas.alpha = 0;
+            canvas.interactable = false;
+            canvas.blocksRaycasts = false;
+        }
+        #endregion
+        #region 事件
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player") && other.name.Contains("Circle")) ;
+            {
+                person_Walk.enabled = false;
+                helpcanva.inputtext = "can you help me??";
+                helpcanva.enabled = true;
+
+
+
+>>>>>>> Stashed changes
             }
 
         }
@@ -27,10 +74,20 @@ namespace Alcor
         {
             if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CircleCollider2D")
             {
+<<<<<<< Updated upstream
                 dialog.SetActive(false);
                 
             }
         }
+=======
+                canvas.alpha = 0;
+                canvas.interactable = false;
+                canvas.blocksRaycasts = false;
+
+            }
+        }
+
+>>>>>>> Stashed changes
         #endregion
 
     }
