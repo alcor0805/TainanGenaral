2a52814767702e233349b4f7ceaa717592599775
alcor0805 <106061646+alcor0805@users.noreply.github.com>
2022-08-06 22:17:26 +0800

    update 整理

---
+++ finalgame/Assets/Script/Alcor/shaft/GameManager_shaft.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+namespace Alcor 
+{
+    public class GameManager_shaft : MonoBehaviour
+    {
+
+        #region 資料
+        #endregion
+        #region 功能
+        #endregion
+        #region 事件
+        public GameObject Setting_canva;
+        public GameObject player;
+        public CameraManager cameraManager;
+        public static bool dead;
+
+        private void Start()
+        {
+            dead = false;
+            Setting_canva.SetActive(false);
+        }
+        private void Update()
+        {
+            if (Player.isDead)
+            {
+                player.SetActive(false);
+                Setting_canva.SetActive(true);
+                cameraManager.enabled = false;
+                Player.isDead = false;
+            }
+        }
+        public void ReloadScene() 
+        {
+            SceneManager.LoadScene(1);
+        }
+        public void MainScene()
+        {
+            dead = true;
+            SceneManager.LoadScene(0);
+        }
+        #endregion
+    }
+}
+
