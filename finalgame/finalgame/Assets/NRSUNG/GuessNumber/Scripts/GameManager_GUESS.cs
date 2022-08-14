d82db518b17c0d366fd1f364da063fbba7b19b88
nrsung <48666091+nrsung@users.noreply.github.com>
2022-08-14 13:31:39 +0800

    update 新增倒數計時

--- finalgame/finalgame/Assets/NRSUNG/GuessNumber/Scripts/GameManager_GUESS.cs
+++ finalgame/finalgame/Assets/NRSUNG/GuessNumber/Scripts/GameManager_GUESS.cs
@@ -12,8 +12,14 @@ namespace NRSUNG
         public int playerAnswer;
         public int correctAnswer;
         public Text hintMessage;
+        public Text gameOverMessage;
+        public Text m_timmer;
+        public int m_min;
+        public int m_sec;
         public Button restartButton;
         public Button exitButton;
+        private bool gameOver;
+        private bool timeOut;
         //public TextMesh hintMessage;
         //public Texture hintMessage;
 
@@ -23,11 +29,26 @@ namespace NRSUNG
             //UpdateHintMessage("請輸入 0~99 的數字");
             NewQuestion();
             FocusPlayerAnswerUI();
+            StartCoroutine(CountDown());
         }
 
-        void Update()
+        private void Update()
         {
+            CheckTimeOut();
+        }
 
+        void CheckTimeOut()
+        {
+            if (timeOut)
+            {
+                UpadteGameOverMessage("闖關失敗 !!!");
+                hintMessage.gameObject.SetActive(false);
+                playerAnswerUI.gameObject.SetActive(false);
+                gameOverMessage.gameObject.SetActive(true);
+                restartButton.gameObject.SetActive(true);
+                exitButton.gameObject.SetActive(true);
+                //StopAllCoroutines();
+            }
         }
 
         void UpdateHintMessage(string message)
@@ -35,12 +56,21 @@ namespace NRSUNG
             hintMessage.text = message;
         }
 
+        void UpadteGameOverMessage(string message)
+        {
+            gameOverMessage.text = message;
+        }
+
         void NewQuestion()
         {
+
             UpdateHintMessage("請輸入 0~99 的數字");
             correctAnswer = Random.Range(0, 100);
             restartButton.gameObject.SetActive(false);
             exitButton.gameObject.SetActive(false);
+            gameOverMessage.gameObject.SetActive(false);
+            playerAnswerUI.gameObject.SetActive(true);
+            gameOver = false;
         }
 
         bool CanGetInputNumber()
@@ -60,8 +90,12 @@ namespace NRSUNG
             if (playerAnswer == correctAnswer)
             {
                 UpdateHintMessage("恭喜你答對了");
+                UpadteGameOverMessage("恭喜你過關了 !!!");
+                playerAnswerUI.gameObject.SetActive(false);
+                gameOverMessage.gameObject.SetActive(true);
                 restartButton.gameObject.SetActive(true);
                 exitButton.gameObject.SetActive(true);
+                StopAllCoroutines();
             }
             else if (playerAnswer > correctAnswer)
             {
@@ -93,6 +127,33 @@ namespace NRSUNG
         void FocusPlayerAnswerUI()
         {
             playerAnswerUI.ActivateInputField();
+        }
+
+        IEnumerator CountDown()
+        {
+            timeOut = false;
+            m_timmer.text = string.Format($"{m_min.ToString("00")}" + " : " + $"{m_sec.ToString("00")}");
+            int m_seconds = (m_min * 60) + m_sec;
+            while (m_seconds > 0 && !timeOut)
+            {
+                yield return new WaitForSeconds(1);
+                m_seconds--;
+                m_sec--;
+
+                if(m_sec<0 && m_min > 0)
+                {
+                    m_min -= 1;
+                    m_sec = 59;
+                }
+                else if (m_sec<0 && m_min == 0)
+                {
+                    m_sec = 0;
+                }
+                m_timmer.text= string.Format($"{m_min.ToString("00")}" + " : " + $"{m_sec.ToString("00")}");
+            }
+            timeOut = true;
+            //yield return new WaitForSeconds(1);
+            //Time.timeScale = 0;
         }
     }
 }
